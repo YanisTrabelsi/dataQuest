f837293c64485d749d4accd72b60f17f9477b82b
YanisTrabelsi <yanis.trabelsi@epitech.digital>
2026-07-03 22:32:49 +0200

    ex0 done

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,13 @@
+import sys
+
+args: list = sys.argv
+len: int = len(args)
+print("=== Command Quest ===")
+print(f"Program name: {args[0]}")
+if (len == 1):
+    print("No arguments provided!")
+else:
+    print(f"Arguments received: {len - 1}")
+    for i in range(len - 1):
+        print(f"Arguments {i + 1}: {args[i + 1]}")
+print(f"Total arguments: {len}")
